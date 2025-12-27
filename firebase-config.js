d056257fcfdd1c581baa4c43b2af7e7e5f02ec06
srija2036 <srijasura6789@gmail.com>
2025-12-27 11:41:58 +0530

    firebase added

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// 1. Initialize Firebase App (Compat Version)
+// These are globally available because of the scripts we will add to your HTML
 const firebaseConfig = {
   apiKey: "AIzaSyB6wWo24uw4jQi3rqBE6CsWBoXFbnePu20",
   authDomain: "izzybizzy-6764a.firebaseapp.com",
@@ -13,5 +9,28 @@ const firebaseConfig = {
   appId: "1:338324518980:web:085f64f51d6067d0466411"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// 2. Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// 3. Initialize Firestore (Database)
+const db = firebase.firestore();
+
+console.log("Izzy Bizzy Firebase is connected! ✨");
+
+/**
+ * Global function to log mood clicks to the database
+ * @param {string} moodName - The name of the mood clicked
+ */
+window.logMood = function(moodName) {
+  db.collection("mood_logs").add({
+    mood: moodName,
+    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    platform: "web"
+  })
+  .then(() => {
+    console.log(`Successfully logged: ${moodName}`);
+  })
+  .catch((error) => {
+    console.error("Error logging mood to Firebase:", error);
+  });
+};
